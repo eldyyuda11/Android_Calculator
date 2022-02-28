c33a8e73e39463b23e09457e468a1b79077268d4
Eldy Yuda Kurniawan <hardsnk11@gmail.com>
2022-02-28 12:39:54 +0800

    membuat backend keypad operator

--- app/src/main/java/project/app/calculator/MainActivity.kt
+++ app/src/main/java/project/app/calculator/MainActivity.kt
@@ -2,10 +2,57 @@ package project.app.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+    private var lastNumeric: Boolean = false
+    private var lastDot: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.TVresult)
+
     }
+
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+    }
+
+    fun onDelete(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric= false
+            lastDot =true
+        }
+    }
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric =false
+                lastDot = false
+            }
+        }
+    }
+    private fun isOperatorAdded(value:String):Boolean{
+            return if (value.startsWith("-"))
+            {
+                return false
+            }else{
+                value.contains("/")||value.contains("*")||value.contains("+")||
+                        value.contains("-")
+
+            }
+    }
+
 }
